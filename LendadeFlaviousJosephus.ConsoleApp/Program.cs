057af2bc02c86f44b2df565b7769c67233c3c00c
Pedro Ataide Silva <Pedro Ataide Silva@DESKTOP-QNHGH3C>
2022-03-03 02:46:32 -0300

    Add project files.

---
+++ LendadeFlaviousJosephus.ConsoleApp/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LendadeFlaviousJosephus.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Digite a quantidade de homens: ");
+            int quantidadeHomens = int.Parse(Console.ReadLine());
+
+            int[] homens = new int[quantidadeHomens + 1];
+
+            Console.Write("Digite o tamanho do salto entre os homens: ");
+            int tamanhoSalto = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= quantidadeHomens; i++)
+            {
+                homens[i] = i;
+            }
+
+            int j = 1, x = 0, y = 0;
+        }
+    }
+}
